22d0c98d483e5965cbc03362bdf2885672c10fad
MichalElektro87 <deltaproc@gmail.com>
2022-12-25 20:43:36 +0100

    Finished company intro animation and this is a stabe version. All distros work.

--- core/src/com/bloodxmas/TitleScreen.java
+++ core/src/com/bloodxmas/TitleScreen.java
@@ -2,16 +2,20 @@ package com.bloodxmas;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.scenes.scene2d.Event;
 
 public class TitleScreen implements Screen {
 
@@ -22,6 +26,7 @@ public class TitleScreen implements Screen {
     private TextButtonStyle textButtonStyle;
     private Texture buttonTexture;
     private NinePatch ninePatch;
+    private TextButton startButton, exitButton;
 
     public TitleScreen(final BloodXmas game) {
         this.game = game;
@@ -34,13 +39,48 @@ public class TitleScreen implements Screen {
         ninePatch = new NinePatch(buttonTexture);
         textButtonStyle.up = new NinePatchDrawable(ninePatch);
         textButtonStyle.font = new BitmapFont(Gdx.files.internal("font/radiospace.fnt"));
+        textButtonStyle.fontColor = Color.GRAY;
+
+        startButton = new TextButton(" Start ", textButtonStyle);
+        exitButton = new TextButton(" Exit ", textButtonStyle);
+
+        startButton.addListener(
+                (Event e) ->
+                {
+                    if (!(e instanceof InputEvent) || !((InputEvent)e).getType().equals(InputEvent.Type.touchDown))
+                        return false;
+
+                    game.setScreen(new IntroScreen(game));
+                    return false;
+                }
+        );
+
+        exitButton.addListener(
+                (Event e) ->
+                {
+                    if (!(e instanceof InputEvent) || !((InputEvent)e).getType().equals(InputEvent.Type.touchDown))
+                        return false;
+
+                    Gdx.app.exit();
+                    return false;
+                }
+        );
 
         uitable = new Table();
         uitable.setFillParent(true);
-        uitable.add(titleSign);
+        //uitable.setDebug(true);
 
+
+        uitable.add(titleSign).center().padBottom(200);
+        uitable.row();
+        uitable.add(startButton).expandX();
+        uitable.add(exitButton).expandX();
+        uitable.row();
 
         stage.addActor(uitable);
+
+        Gdx.input.setInputProcessor(stage);
+
     }
 
     @Override
